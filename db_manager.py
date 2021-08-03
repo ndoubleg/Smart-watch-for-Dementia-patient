92db115275ce630bf7317a9646c5e3302b612341
SeokJoo Cho <lnxlht4j@gmail.com>
2021-08-04 04:24:12 +0900

    updated to be compatible with mysql database (not sqlite)

--- db_manager.py
+++ db_manager.py
@@ -1,15 +1,36 @@
-import sqlite3
+import pymysql
+from pymysql.cursors import DictCursor
+
 
 class DatabaseManager:
     connection = None
     cursor = None
-    DB_PATH = "~/*.db"
+    DB_PATH = "~/credentials_db.sql"
 
     def create_connection(self):
+        """ Create connection with database """
         try:
             print(f"Connecting to database : {self.DB_PATH} ...")
-            self.connection = sqlite3.connect(self.DB_PATH)
-            print(f"Succesfully connected to database : {self.DB_PATH}")
-        except sqlite3.Error as e:
+            self.connection = pymysql.connect(database=self.DB_PATH)
+            print(f"Successfully connected to database : {self.DB_PATH}")
+        except pymysql.Error as e:
             print(e)
 
+    def get_cursor(self):
+        """ Create cursor """
+        self.cursor = self.connection.cursor(DictCursor)
+
+    def close_connection(self):
+        """ Close connection to database """
+        if self.connection is not None:
+            self.connection.close()
+            print(f'Succesfully closed database : {self.DB_PATH}')
+
+    def select_column_with_filter(self, column_name, filter_keyword, table_name):
+        """Fetch all records with 'filter_keyword' inside 'column_name' """
+        query = f"""
+        SELECT {column_name} 
+        FROM {table_name} 
+        WHERE {column_name} LIKE '%{filter_keyword}%'
+        """
+        self.cursor.execute(query)
