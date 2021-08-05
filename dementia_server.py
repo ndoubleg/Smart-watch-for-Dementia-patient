2b1e0a04eb7251629852011f30ac19b1181657eb
Ubuntu <ubuntu@ip-172-31-6-167.ap-northeast-2.compute.internal>
2021-08-05 14:34:56 +0000

    added 'address' route to Flask

--- dementia_server.py
+++ dementia_server.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-    return "Dementia Server access succeed"
+    return "dementia server"
 
+
+@app.route('/address',methods=['GET','POST'])
+def address_request():
+    return render_template("daum.html")
 app.run(host="0.0.0.0", port=5000, debug=True)
