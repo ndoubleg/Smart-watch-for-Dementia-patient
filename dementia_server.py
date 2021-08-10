25e3bed1dd376318ae409241ee7d31aa30a79b79
Seokjoo Cho <lnxlht4j@gmail.com>
2021-08-10 17:11:47 +0900

    changed root route return request for test purpose (2)

--- dementia_server.py
+++ dementia_server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-    return request
+    return request.values
 
 
 @app.route('/append-location', methods=['GET', 'POST'])
